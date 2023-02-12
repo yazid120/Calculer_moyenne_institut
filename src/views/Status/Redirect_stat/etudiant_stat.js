2e184c8757d6f6a89a2b427521b36bda7909a31b
Kevin_makalister <74493602+yazid120@users.noreply.github.com>
2023-02-13 00:55:00 +0100

    N

--- src/views/Status/Redirect_stat/etudiant_stat.js
+++ src/views/Status/Redirect_stat/etudiant_stat.js
@@ -5,11 +5,38 @@ class Etudiant_stat extends Component{
 
 
     render(){
+         let Sets_Object=[
+            {F_name:'First Name'},
+            {L_name:'Last Name'},
+            {Num_inscr:'registration number'},
+            {Sub_sets_btn:'Finish'}
+         ]
         return( 
             <>
             <div>
                 <h1>Stagier page infos remplir:</h1>
             </div>
+
+            <form method="post" className="wrapp_form_setStat_user">
+                <label htmlFor="" className="foreign_side_cl">
+                    {Sets_Object[0].F_name}
+                </label>
+                <input type="text" className="Status_input_Vle"/>
+
+                <label htmlFor="" className="foreign_side_cl">
+                   {Sets_Object[1].L_name}
+                </label>
+                <input type="text" className="Status_input_Vle"/>
+
+                <label htmlFor="" className="foreign_side_cl">
+                  {Sets_Object[2].Num_inscr}
+                </label>
+                <input type="text" className="Status_input_Vle"/>
+
+                <button type="submit" className="">
+                  {Sets_Object[3].Sub_sets_btn}
+                </button>
+            </form>
             </>
         )
     }
--- src/views/Status/Redirect_stat/prof_stat.js
+++ src/views/Status/Redirect_stat/prof_stat.js
@@ -5,11 +5,32 @@ class Prof_Status extends Component{
 
 
     render(){
+        let Sets_Object=[
+            {F_name:'First Name'},
+            {L_name:'Last Name'},
+            {Sub_sets_btn:'Finish'}
+         ]
         return( 
             <>
             <div>
                 <h1>prof page infos remplir:</h1>
             </div>
+
+            <form method="post" className="wrapp_form_setStat_user">
+                <label htmlFor="" className="foreign_side_cl">
+                    {Sets_Object[0].F_name}
+                </label>
+                <input type="text" className="Status_input_Vle"/>
+
+                <label htmlFor="" className="foreign_side_cl">
+                   {Sets_Object[1].L_name}
+                </label>
+                <input type="text" className="Status_input_Vle"/>
+
+                <button type="submit" className="">
+                  {Sets_Object[2].Sub_sets_btn}
+                </button>
+            </form>
             </>
         )
     }
