f3d793668a9424f74bb5b85d4db5138e6683731c
YazidZed12 <125869780+YazidZed12@users.noreply.github.com>
2023-02-27 00:39:11 +0100

    C

--- src/views/profile/component_profile/Dashboard.jsx
+++ src/views/profile/component_profile/Dashboard.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import SideBar from "./SideBar";
+import { FcAdvertising, FcFinePrint,FcFlashOn } from "react-icons/fc"; 
 import '../../../style/Dashboard.css'; 
 
 
@@ -10,6 +11,48 @@ let Dashboard = function(){
     <SideBar/>
         <div className="dash_wrapp_content">
             <h1>Dashboard page</h1>
+        
+         <div className="main_DashBoard_container_elements">
+
+
+        <div className="block_dual_listed">
+            <div className="Valid_usage_platform _Cpr">
+                <h2 className="Fon_tier">
+                    peroformence <FcFlashOn/>
+                </h2>
+
+            </div>
+
+            <div className="annonce_usage_solid _Cpr">
+                <h2 className="Fon_tier">
+                    announce <FcAdvertising/>
+                </h2>
+            </div>
+        </div>
+
+
+        <div className="block_dual_listed">
+            <div className="nouveaute_usage_publication _Cpr">
+                <h2 className="Fon_tier">nouveauté</h2>
+            </div>
+
+            <div className="Sortie_date_resultat _Cpr">
+                <h2 className="Fon_tier">les dates de sortie des results</h2>
+
+            </div>
+        </div>
+              
+        <div className="block_dual_listed">
+            <div className="Profile_user_part _Cpr">
+                <h2 className="Fon_tier">Profile</h2>
+            </div>
+            <div className="complete_spinner_bnner _Cpr">
+                <h2 className="Fon_tier">Completer votre profile <FcFinePrint/></h2>
+            </div>
+        </div>
+
+
+        </div>
         </div>
     </div>    
         
