b7483ac28108850a848b52f2343719930b02c4a8
Kevin_makalister <74493602+yazid120@users.noreply.github.com>
2023-03-06 00:12:46 +0100

    C

--- src/views/profile/component_profile/Prof_profile_component/Section_prof_com/Modify_section_prof.js
+++ src/views/profile/component_profile/Prof_profile_component/Section_prof_com/Modify_section_prof.js
@@ -1,22 +1,35 @@
 import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from 'axios';
 
 
 
 let Modify_section = function(){
   const [ Data_section, Set_section] = useState({
     nom_sec:'',
-    Spe_sec:''
+    spec_sec:''
   }); 
+  const navigate = useNavigate(); 
 
+  const {nom_sec,spec_sec} = Data_section; 
 
+  let Handle_update = (e)=>{
+    Set_section({...Data_section,[e.target.name]:e.target.value});
+  }
 
+  let Update_section = (e)=>{
+    e.preventDefault();
+    console.log(Data_section);
+  
+
+  }
 
 
   let Modify_Section_Object=[
     {lab_01:'nom Section'},
     {lab_02:'identifient Section'},
     {lab_04:'Specialites de la Section'},
-    {Modifier_sect:'Ouvrir la modification de cette section'}
+    {Modifier_sect:'Modifier cette section'}
   ]
   return( 
   <>
@@ -28,14 +41,13 @@ let Modify_section = function(){
           {Modify_Section_Object[0].lab_01}
         </label>
         <input type='text' className="manage_section_cr" 
-        name='name_section' />
+        name='nom_sec' value={nom_sec} onChange={Handle_update}/>
 
         <label>
         {Modify_Section_Object[3].lab_04}
         </label>
         <select className="Selection_specialties" name="spec_sec"
-        // value={spec_sec} 
-        // onChange={Change_Handler_Section}
+        value={spec_sec}  onChange={Handle_update}
         >
           <option>Specialites</option>
           <option>Développement WEB</option>
@@ -45,7 +57,8 @@ let Modify_section = function(){
         </select>
 
       <div className="Submition_container">
-        <button type='submit' className="submition_new_section _Modify_section">
+        <button type='submit' className="submition_new_section _Modify_section"
+        onClick={Update_section}>
           {Modify_Section_Object[3].Modifier_sect}
         </button>
       </div>
